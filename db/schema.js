574ad1a6a3d68cdfdc842b4bc45210ec8f76c216
JasenABaker <JasenABaker@gmail.com>
2018-01-03 19:52:53 -0500

    created Dungeon master schema

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,41 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema()
 
 mongoose.Promise = global.Promise
+
+
+const DungeonMasterSchema = new Schema (
+    {
+        firstName:{
+            type: String,
+            required: true,
+            unique: false
+        },
+        lastName: {
+            type: String,
+            required: true,
+            unique: false
+        },
+        username:{
+            type: String,
+            required: true,
+            unique: true
+        },
+        photoUrl:{
+            type: String,
+            default: 'http://pre06.deviantart.net/74dc/th/pre/i/2013/046/6/8/dungeon_master_by_axlsalles-d5v1nco.jpg',
+            required: false,
+            unique: false,
+
+        },
+        adventure: [ adventureSchema]
+
+    },
+    {
+        timestamps:{}
+    }
+)
+
+
+module.exports ={
+    DungeonMasterSchema
+}
