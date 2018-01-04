5dfe263a23192737f02306f2b279a30cf976978a
JasenABaker <JasenABaker@gmail.com>
2018-01-04 16:41:36 -0500

    added Dm NEW POST route

--- routes/DmController.js
+++ routes/DmController.js
@@ -4,38 +4,53 @@ const router = express.Router();
 const DungeonMaster = require('../db/models/Dm')
 
 
-router.get('/', (req,res) =>{
+router.get('/', (req, res) => {
     DungeonMaster.find()
-    .then((DMs)=>{
-        res.render('dm/index', {
-            DMs,
-            title: 'Dungeon Masters'
+        .then((DMs) => {
+            res.render('dm/index', {
+                DMs,
+                title: 'Dungeon Masters'
+            })
         })
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+        .catch((err) => {
+            console.log(err)
+        })
 })
 
-router.get('/new', (req,res) =>{
-    res.render('Dm/new', { title: 'New Dungeon Master'})
+router.get('/new', (req, res) => {
+    res.render('Dm/new', { title: 'New Dungeon Master' })
 })
 
-router.get('/:DmId', (req,res)=>{
+
+router.post('/', (req, res) => {
+    const newDm = req.body
+    DungeonMaster.create(newDm)
+        .then((Dm) => {
+            res.redirect('/Dm')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+router.get('/:DmId', (req, res) => {
     const DmId = req.params.DmId
 
     DungeonMaster.findById(DmId)
-    .then((Dm)=>{
-        res.render('dm/show', {
-            DmId,
-            Dm,
-            title: `${Dm.username}`
+        .then((Dm) => {
+            res.render('dm/show', {
+                DmId,
+                Dm,
+                title: `${Dm.username}`
+            })
         })
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+        .catch((err) => {
+            console.log(err)
+        })
 })
+
+
+
 
 
 
