102f59c7e5c0f11b9a5ca0bdc1a9681a572e41a5
JasenABaker <JasenABaker@gmail.com>
2018-01-03 20:40:29 -0500

    updated index.js to indexControllers.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const index = require('./routes/index');
+const indexController = require('./routes/indexController');
 const users = require('./routes/users');
 
 const app = express();
@@ -41,7 +41,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', indexController);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
