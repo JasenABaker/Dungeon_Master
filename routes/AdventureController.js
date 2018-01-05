4f8d5cf3949e01ace2572edbeb32ec68d3f58ff1
JasenABaker <JasenABaker@gmail.com>
2018-01-05 11:21:28 -0500

    Added Adventure Index GET route

--- routes/AdventureController.js
+++ routes/AdventureController.js
@@ -1,5 +1,31 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
-
+const DungeonMaster = require('../db/models/Dm')
 const Adventure = require('../db/models/Adventure')
+
+router.get('/', (req, res)=>{
+    const DmId = req.params.DmId
+
+    DungeonMaster.findById(DmId)
+    .then((Dm)=>{
+        res.render('Adventures/index'. {
+            adventures: Dm.adventures,
+            DmId: Dm._id,
+            title: `${Dm.username}'s Adventures`
+        })
+    }).catch((err)=>{
+        console.log(err)
+    })
+
+})
+
+
+
+
+
+
+
+
+
+module.exports = router
