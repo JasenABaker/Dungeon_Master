01dab8fff6b84e6c8ae92d6f04715e4f801ea5ad
JasenABaker <JasenABaker@gmail.com>
2018-01-06 12:19:32 -0500

    added new player GET route

--- routes/PlayerController.js
+++ routes/PlayerController.js
@@ -23,6 +23,24 @@ router.get('/', (req, res) =>{
     })
 })
 
+router.get('/new', (req, res) =>{
+    const DmId = req.params.DmId
+    const AdvenId = req.params.AdvenId
+
+    DungeonMaster.findById(DmId)
+    .then((Dm)=>{
+        const adventure = Dm.adventure.id(AdvenId)
+        res.render('players/new', {
+            DmId,
+            adventure,
+            title: 'Add Player to:'
+        })
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
+
 
 
 
