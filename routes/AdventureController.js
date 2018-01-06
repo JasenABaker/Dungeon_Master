84d7ba83559e02498cbfa18be5de0939a792eed1
JasenABaker <JasenABaker@gmail.com>
2018-01-06 17:31:56 -0500

    more styling

--- routes/AdventureController.js
+++ routes/AdventureController.js
@@ -56,6 +56,7 @@ router.get('/:AdvenId', (req,res) => {
         const adventure = Dm.adventures.id(AdvenId)
         
         res.render('adventures/show', {
+            Dm,
             DmId,
             adventure,
             title: `${adventure.name}`
