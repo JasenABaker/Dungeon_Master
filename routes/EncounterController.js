0842fbcdb525e8ab5185064b7eab90eace54dc91
JasenABaker <JasenABaker@gmail.com>
2018-01-07 01:05:47 -0500

    added styling to Encounter Index page

--- routes/EncounterController.js
+++ routes/EncounterController.js
@@ -14,6 +14,7 @@ router.get('/', (req, res)=>{
             const encounters = adventure.encounters
 
             res.render('encounters/index', {
+                Dm,
                 DmId,
                 AdvenId,
                 adventure,
