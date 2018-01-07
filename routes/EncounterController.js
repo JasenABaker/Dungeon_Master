1672dd4e93be8ce15823f2c7890bdb8c40853d84
JasenABaker <JasenABaker@gmail.com>
2018-01-07 00:42:00 -0500

    add encounter index GET route

--- routes/EncounterController.js
+++ routes/EncounterController.js
@@ -4,6 +4,30 @@ const router = express.Router({mergeParams: true});
 const DungeonMaster = require('../db/models/Dm')
 const Adventure = require('../db/models/Adventure')
 
+router.get('/', (req, res)=>{
+    const DmId = req.params.DmId
+    const AdvenId = req.params.AdvenId
+
+    DungeonMaster.findById(DmId)
+        .then((Dm) => {
+            const adventure = Dm.adventures.id(AdvenId)
+            const encounters = adventure.encounters
+
+            res.render('encounters/index', {
+                DmId,
+                AdvenId,
+                adventure,
+                encounters,
+                title: `Encounters`
+            })
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+
+    
+})
+
 
 
 
