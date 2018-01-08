8af61ceba1fc65f32663e5f12e8d785a5beef737
JasenABaker <JasenABaker@gmail.com>
2018-01-07 21:38:20 -0500

    created monster DELETE route

--- routes/MonsterController.js
+++ routes/MonsterController.js
@@ -57,6 +57,29 @@ router.get('/new', (req, res) => {
         })
 })
 
+router.get('/:MonstId/delete', (req, res)=>{
+    const DmId = req.params.DmId
+    const AdvenId = req.params.AdvenId
+    const EncountId = req.params.EncountId
+    const MonstId = req.params.MonstId
+    
+    DungeonMaster.findById(DmId)
+    .then((Dm)=>{
+        const adventure = Dm.adventures.id(AdvenId)
+        const encounter = adventure.encounters.id(EncountId)
+        encounter.monsters.id(MonstId).remove()
+        
+        return Dm.save()
+    })
+    .then(()=>{
+        res.redirect(`/Dm/${DmId}/adventures/${AdvenId}/encounters/${EncountId}/monsters`)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+
+})
+
 
 router.post('/', (req, res)=>{
     const DmId = req.params.DmId
